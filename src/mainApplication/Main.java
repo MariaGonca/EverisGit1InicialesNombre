ccc19f143815e45370221e0b3d141aa19e3b6b2e
Maria Del Carmen <mcgcarranza@hotmail.es>
2022-06-30 21:43:23 +0200

    hola desarrollador creado

--- src/mainApplication/Main.java
+++ src/mainApplication/Main.java
@@ -1,9 +1,20 @@
 package mainApplication;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String nombreDesarrollador;
+		
+		System.out.println("Digame su nombre");
+		nombreDesarrollador = sc.next();
+		
+		System.out.println("Hola " + nombreDesarrollador);
 	}
 
 }
